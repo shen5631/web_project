9c2b3f69d0e0e935c87cdb9785bcf8bba2efad59
shen <shen5631@naver.com>
2026-09-18 11:36:29 +0900

    개발자 계정 추가 및 일반 사용자 접근 제어

--- js/index.js
+++ js/index.js
@@ -2,3 +2,13 @@ const name = localStorage.getItem("user_name");
 document.getElementById("name").innerText = name + " 학생";
 
 console.log("!");
+
+function test() {
+  const check = localStorage.getItem("user_auth");
+  if (check === "false") {
+    alert("게시물 생성 권한이 없습니다.");
+    console.log("!");
+  } else {
+    location.href = "add_dong.html";
+  }
+}
